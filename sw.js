294ecc93a9a8458c4b240b153658b766b0a1cfe1
iwannabekoshka <pokusaeff.daniil@gmail.com>
2025-03-28 15:23:09 +0300

    Revert "autoupdate"
    This reverts commit f4fc06bd6c75f017ff9e2207a0dbaf2116f94de1.

--- sw.js
+++ sw.js
@@ -1,58 +1,45 @@
 const CACHE_NAME = "pwa-form-cache-v11";
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/trials.html",
-  "/icons/pvs_logo_4.png",
-  "/static/css/style.css",
-  "/static/fonts/Manrope/Manrope-VariableFont_wght.ttf",
-  "/static/fonts/Pribambas/WEB/Pribambas-Regular.woff2",
-  "/static/fonts/Pribambas/WEB/Pribambas-Regular.woff",
-  "/static/images/delete-48.png",
-  "/static/images/excel-3-48.png",
-  "/static/js/app.js",
-  "/static/js/confetti.js",
-  "/static/js/form.js",
-  "/static/js/trials.js",
+    '/',
+    '/index.html',
+    '/trials.html',
+    '/icons/pvs_logo_4.png',
+    '/static/css/style.css',
+    '/static/fonts/Manrope/Manrope-VariableFont_wght.ttf',
+    '/static/fonts/Pribambas/WEB/Pribambas-Regular.woff2',
+    '/static/fonts/Pribambas/WEB/Pribambas-Regular.woff',
+    '/static/images/delete-48.png',
+    '/static/images/excel-3-48.png',
+    '/static/js/app.js',
+    '/static/js/confetti.js',
+    '/static/js/form.js',
+    '/static/js/trials.js',
 ];
 
-self.addEventListener("install", function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+    );
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys
-          .filter((key) => key !== CACHE_NAME)
-          .map((key) => caches.delete(key))
-      );
-    })
-  );
-});
-
-self.addEventListener("install", (event) => {
-  self.skipWaiting(); // Активирует новый SW сразу
-});
-self.addEventListener("activate", (event) => {
-  clients.claim(); // Применяет новый SW ко всем вкладкам
-});
-
-self.addEventListener("message", (event) => {
-  if (event.data === "skipWaiting") {
-    self.skipWaiting();
-  }
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+            );
+        })
+    );
 });
